7b73f7bf7c2139eb9a2cbda70daaba0168bd20dd
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-01-11 19:11:38 +1000

    Some more foundational C++ work

--- linalg/matvecmul.cpp
+++ linalg/matvecmul.cpp
@@ -8,6 +8,28 @@ using namespace Rcpp;
 //-----------------------------------
 
 // [[Rcpp::export]]
-NumericMatrix matvecmul(NumericMatrix A){
-  x
+NumericVector matvecmul(NumericMatrix A, NumericVector b){
+  
+  // Check if # cols in 1st matrix matches # rows in 2nd matrix
+  
+  int rowA = A.nrow();
+  int colA = A.ncol();
+  int rowb = b.nrow();
+  int colb = b.ncol();
+  
+  if(colA != rowb){
+    Rcpp::stop("Number of columns in matrix A should be equal to the number of components in vector B.");
+  }
+  
+  // Multiply correct elements of both to produce new vector with i rows and 1 column
+  // This is row i and col j from A * row j from B
+  
+  for(int i = 0; i < rowA; i++){
+    // Instantiate new vector to be empty
+    c[i] = 0; 
+    for(int j = 0; j < colb; j++){
+        c[i] += A[i][j]*b[j];
+      }
+    }
+  return c;
 }
